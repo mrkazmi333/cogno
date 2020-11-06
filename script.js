8fb49891ebc9ab3e0c485189f2feef87ef8aa0ea
mrkcancode <motasimkazmi333@gmail.com>
2020-11-06 19:52:37 +0530

    chnaged styles

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 var iframe = document.getElementById("iframe");
 var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 const openBotButton = document.querySelector("#bot-open-img img");
-var upperPart = document.getElementById("inner-part");
-// const upperInfo = innerDoc.querySelector("#upper-info-container");
-
-console.log(innerDoc);
-console.log(upperPart);
-// console.log(iframe);
-// console.log(openBotButton);
 
 openBotButton.addEventListener("click", (e) => {
   e.preventDefault();
